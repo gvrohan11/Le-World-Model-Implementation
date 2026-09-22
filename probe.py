ef24ebc978b1086e4c7dcb282ab34073a97f270e
Rohan Gudipaty <gvrohan11@gmail.com>
2026-09-22 09:01:57 +0000

    modified typo

--- probe.py
+++ probe.py
@@ -25,7 +25,7 @@ with h5py.File(DATASET, "r") as f:
 imgs = torch.from_numpy(imgs).permute(0, 3, 1, 2).float() / 255.0
 Y = torch.from_numpy(jpos).float()
 train_mean = Y[tr].mean(0, keepdim=True)
-train_std = Y[tr].stf(0, keepdim=True).clamp_min(1e-6)
+train_std = Y[tr].std(0, keepdim=True).clamp_min(1e-6)
 # Yn = (Y - Y.mean(0, keepdim=True)) / (Y.std(0, keepdim=True) + 1e-6)   # standardize targets
 Yn = (Y - train_mean) / train_std
 print("probing on", len(imgs), "frames")
